9e655c0ba5f2c15ef360af0895b5233d40e9a6e6
Wilson <mkdirjava@gmail.com>
2019-02-08 17:47:17 +0000

    solved problem 1, recursion issue

---
+++ src/main/scala/com/practice/projectEuler/Problem1.scala
@@ -0,0 +1,27 @@
+package com.practice.projectEuler
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+object Problem1 {
+
+  def divisableBy3 (number:Int):Boolean = number % 3 == 0
+  def divisableBy5 (number:Int):Boolean = number % 5 == 0
+
+  def sumOfdivisableBy3And5 (maxNumber:Int, numberIteration:Int = 0):Int={
+
+    numberIteration < maxNumber match{
+      case true =>{
+        if(divisableBy3(numberIteration) || divisableBy5(numberIteration) ){
+          numberIteration + sumOfdivisableBy3And5(maxNumber,numberIteration+1)
+        }else{
+          sumOfdivisableBy3And5(maxNumber,numberIteration+1)
+        }
+      }
+      case false => 0
+    }
+
+  }
+
+}
---
+++ src/test/scala/com/practice/projectEuler/Problem1_Test.scala
@@ -0,0 +1,16 @@
+package com.practice.projectEuler
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+@Test
+class Problem1_Test {
+
+  @Test
+  def problem1Test = {
+    println(Problem1.sumOfdivisableBy3And5(1000))
+    assertTrue(1==1)
+
+  }
+
+}
